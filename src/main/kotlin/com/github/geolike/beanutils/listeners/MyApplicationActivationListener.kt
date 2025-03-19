9875769a8a2f4cc1093ba64df991d414e5f8f9bc
GitHub Action <action@github.com>
2025-03-19 06:38:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/geolike/beanutils/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/geolike/beanutils/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.geolike.beanutils.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
